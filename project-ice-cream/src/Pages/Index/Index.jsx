07ac948b37213955985f5801be5012d6ea28cf87
Darshani24 <radkumari@std.appsc.sab.ac.lk>
2025-05-09 15:04:19 +0530

    Import New Images

--- project-ice-cream/src/Pages/Index/Index.jsx
+++ project-ice-cream/src/Pages/Index/Index.jsx
@@ -6,7 +6,14 @@ import "swiper/css";
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import { Autoplay } from 'swiper/modules';
-
+import SlideImage1 from './../../assets/SlideImage1.jpg'
+import SlideImage2 from './../../assets/SlideImage2.jpg'
+import SlideImage3 from './../../assets/SlideImage3.jpg'
+import SlideImage4 from './../../assets/SlideImage4.jpg'
+import SlideImage5 from './../../assets/SlideImage5.jpg'
+import SlideImage6 from './../../assets/SlideImage6.jpg'
+import SlideImage7 from './../../assets/SlideImage7.jpg'
+import SlideImage8 from './../../assets/SlideImage8.jpg'
 
 
 
